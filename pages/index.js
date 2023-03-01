1728795d8a3a7e5bf6a013660e41995a3957e294
sekudash <126599592+sekudash@users.noreply.github.com>
2023-03-01 21:28:53 +0000

    AIHenryBranch

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,105 @@
+// Add useState import to top of file
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  // Don't retry more than 20 times
+  const maxRetries = 20;
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState('');
+  const [retry, setRetry] = useState(0);
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState('');
+
+  // Add this function
+  const onChange = (event) => {
+    setInput(event.target.value);
+    };
+  
+  // Add generateAction
+  const generateAction = async () => {
+    console.log('Generating...');
+
+    // Add this check to make sure there is no double click
+    if (isGenerating && retry === 0) return;
+    
+    // Set loading has started
+    setIsGenerating(true);
+
+    // If this is a retry request, take away retryCount
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+    }
+
+    const finalInput = input.replace("Ashton", "aHenryDukes")
+    
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input: finalInput }),
+    });
+
+    const data = await response.json();
+
+    if (response.status === 503) {
+      // Set the estimated_time property in state
+      setRetry(data.estimated_time);
+      return;
+    }
+
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      // Stop loading
+      setIsGenerating(false);
+      return;
+    }
+
+    setFinalPrompt(input);
+    setInput('');
+    setImg(data.image);
+    // Everything is all done -- stop loading!
+    setIsGenerating(false);
+  };
+    // Add useEffect here
+    const sleep = (ms) => {
+      return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+      });
+    };
+    useEffect(() => {
+      const runRetry = async () => {
+        if (retryCount === 0) {
+          console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+          setRetryCount(maxRetries);
+          return;
+          }
+  
+        console.log(`Trying again in ${retry} seconds.`);
+  
+        await sleep(retry * 1000);
+  
+        await generateAction();
+      };
+  
+      if (retry === 0) {
+        return;
+      }
+  
+      runRetry();
+    }, [retry]);
   return (
     <div className="root">
       <Head>
@@ -11,13 +108,42 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>An Ashton Henry Image Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Generate images of Ashton with text below! Make sure to use the capitalized reference "Ashton" in the prompt</h2>
+          </div>
+          {/* Add prompt container here */}
+        <div className="prompt-container">
+          <input className="prompt-box" value={input} onChange={onChange} />
+          <div className="prompt-buttons">
+            {/* Tweak classNames to change classes */}
+            <a 
+              className={
+                  isGenerating ? 'generate-button loading' : 'generate-button'
+              }
+              onClick={generateAction}
+            >
+              {/* Tweak to show a loading indicator */}
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                <p>Generate</p>
+                )}
+              </div>
+            </a>
           </div>
         </div>
       </div>
+      {/* Add output container*/}
+      {img && (
+        <div className="output-content">
+          <Image src={img} width={512} height={512} alt={input} />
+          <p>{finalPrompt}</p>
+        </div>
+      )}
+    </div>
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-avatar"
@@ -26,7 +152,7 @@ const Home = () => {
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>built with buildspace + md</p>
           </div>
         </a>
       </div>
